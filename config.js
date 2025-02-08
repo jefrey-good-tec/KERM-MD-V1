4892851c86f8945c62edd145988e023fbf839477
jefrey-good-tec <dahmerjefrey4@gmail.com>
2025-02-08 17:57:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+SESSION_ID: process.env.SESSION_ID || "7dZjCRQZ#2JSsV33amUf8iQ0kEotdvMMPrfZffBwgIVfbFGZmsrQ",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
